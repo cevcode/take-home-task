e897ab9a85720bcc8baeade429537c4c88f5c095
cevcode <kobernikev@gmail.com>
2022-08-13 14:45:51 +0200

    feat: include fonts, made form styling

--- src/helpers/theme.ts
+++ src/helpers/theme.ts
@@ -6,29 +6,84 @@ export const globalStyles = {
         secondaryText: '#708797',
         accentText: '#0079FF',
         accent: '#1B31A8',
+        borderColor: '#E9EEF2',
     },
     fonts: {
-        default: `font-family: 'WorkSans', sans-serif`,
-        accent: `font-family: 'Rubik', sans-serif`,
+        default: `'WorkSans', sans-serif`,
+        accent: `'Rubik', sans-serif`,
     }
 };
 
 const fontsConfig = [
     {
         name: 'Rubik',
-        src: 'Rubik/Rubik',
+        src: 'Rubik/Rubik-Light',
+        fontWeight: 300,
+    },
+    {
+        name: 'Rubik',
+        src: 'Rubik/Rubik-Regular',
+        fontWeight: 400,
+    },
+    {
+        name: 'Rubik',
+        src: 'Rubik/Rubik-Medium',
+        fontWeight: 500,
+    },
+    {
+        name: 'Rubik',
+        src: 'Rubik/Rubik-SemiBold',
+        fontWeight: 600,
+    },
+    {
+        name: 'Rubik',
+        src: 'Rubik/Rubik-Bold',
+        fontWeight: 700,
+    },
+    {
+        name: 'Rubik',
+        src: 'Rubik/Rubik-Black',
+        fontWeight: 800,
+    },
+
+    {
+        name: 'WorkSans',
+        src: 'WorkSans/WorkSans-Light',
+        fontWeight: 300,
     },
     {
         name: 'WorkSans',
-        src: 'WorkSans/WorkSans',
+        src: 'WorkSans/WorkSans-Regular',
+        fontWeight: 400,
+    },
+    {
+        name: 'WorkSans',
+        src: 'WorkSans/WorkSans-Medium',
+        fontWeight: 500,
+    },
+    {
+        name: 'WorkSans',
+        src: 'WorkSans/WorkSans-SemiBold',
+        fontWeight: 600,
+    },
+    {
+        name: 'WorkSans',
+        src: 'WorkSans/WorkSans-Bold',
+        fontWeight: 700,
+    },
+    {
+        name: 'WorkSans',
+        src: 'WorkSans/WorkSans-Black',
+        fontWeight: 800,
     },
 ];
 
-function fontFace(name: string, src: string) {
+function fontFace(name: string, src: string, fontWeight: number) {
     return `
         @font-face{
             font-family: '${name}';
             font-style: normal;
+            font-weight: ${fontWeight};
             src: url(${'/fonts/' + src + '.eot'});
             src: url(${'/fonts/' + src + '.eot'}?#iefix) format('embedded-opentype'),
                  url(${'/fonts/' + src + '.woff'}) format('woff'),
@@ -40,8 +95,8 @@ function fontFace(name: string, src: string) {
 
 export const withFonts = () => {
     return fontsConfig.map((item) => {
-        const { name, src } = item;
-        return fontFace(name, src);
+        const { name, src, fontWeight } = item;
+        return fontFace(name, src, fontWeight);
     });
 };
 
@@ -50,6 +105,7 @@ export const GlobalStyle = createGlobalStyle`
     ${withFonts()};
     background-color: #F4F8FA;
     color: ${globalStyles.palette.mainText};
+    font-family: ${globalStyles.fonts.default};
     margin: 0;
     width: 100%;  
     padding: 0;
@@ -60,5 +116,26 @@ export const GlobalStyle = createGlobalStyle`
   }
   *, :after, :before {
     box-sizing: border-box;
+  }
+  p {
+    font-size: 1rem;
+  }
+  h1 {
+    font-size: 4.5rem;
+  }
+  h2 {
+    font-size: 4rem;
+  }
+  h3 {
+    font-size: 3rem;
+  }
+  h4 {   
+    font-size: 2.25rem;
+  }
+  h5 {
+    font-size: 1.75rem;
+  }
+  h6 {
+    font-size: 1.5rem;
   }
 `;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,51 +2,167 @@ import React from 'react';
 import logoImg from '~/static/logoOrigin.svg';
 import homeImg from '~/static/home_icon.svg';
 import styled from 'styled-components';
+import { globalStyles } from '~/helpers/theme';
 
 const StyledPage = styled.div`
-  min-height: 100vh;
+    min-height: 100vh;
 `;
 
 const StyledHeader = styled.div`
-  width: 100%;
+    width: 100%;
+    background-color: #fff;
+    padding: 1.5rem 3.5rem;
+`;
+
+const StyledOuter = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const StyledInner = styled.div`
+    width: 35rem;
+`;
+
+const StyledDescription = styled.p`
+    font-size: 1.25rem;
+    color: ${globalStyles.palette.accent};
+    padding: 1.5rem 0;
+    text-align: center;
+`;
+
+const StyledBox = styled.div`
   background-color: #fff;
-  padding: 1.5rem 3.5rem;
+  box-shadow: 0 16px 32px rgba(30, 42, 50, 0.08);
+  border-radius: 0.5rem;
+  padding: 2.5rem;
+`;
+
+const StyledBold = styled.span`
+  font-weight: 600;
+`;
+
+const StyledBoxHeader = styled.div`
+  display: inline-flex;
+  align-items: center;
+  img {
+    width: 4rem;
+  }
+`;
+
+const StyledRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const StyledAmount = styled.div`
+  border: 1px solid ${globalStyles.palette.borderColor};
+  border-radius: 0.5rem;
+`;
+
+const StyledAmountTop = styled(StyledRow)`
+  padding: 1.5rem;
+  align-items: center;
+  p:first-child {
+    font-size: 1.25rem;
+  }
+`;
+
+const StyledAmountBottom = styled(StyledRow)`
+  background-color: #F4F8FA;
+  padding: 1.5rem;
+  p {
+    font-size: 0.75rem;
+  }
+`;
+
+
+const StyledAmountPrice = styled.p`
+  color: ${globalStyles.palette.accentText};
+  font-size: 2rem;
+  font-family: ${globalStyles.fonts.accent};
+  font-weight: 500;
+`;
+
+const StyledBoxHeaderContent = styled.div`
+  margin-left: 1rem;
+  h6 {
+    font-family: ${globalStyles.fonts.accent};
+    font-weight: 500;
+  }
+  p {
+    color: ${globalStyles.palette.secondaryText}
+  }
+`;
+
+const StyledButton = styled.button`
+  width: 320px;
+  padding: 1.125rem 0;
+  color: #FFF;
+  font-weight: 600;
+  outline: 0;
+  border-radius: 2rem;
+  background-color: ${globalStyles.palette.accent};
+  border: none;
+  cursor: pointer;
+  &:hover {
+    filter: grayscale(30%);
+  }
+  &:active {
+    filter: grayscale(50%);
+  }
+`;
+
+const StyledFooter = styled(StyledRow)`
+  align-items: center;
+  margin-top: 2rem;
+  justify-content: center;
 `;
 
 const HomePage = () => {
-  return (
-      <StyledPage>
-          <StyledHeader>
-              <img src={logoImg} alt="logo"/>
-          </StyledHeader>
-          <div>
-              <p>Let's plan your <b>saving goal</b></p>
-              <div>
-                  <div>
-                      <img src={homeImg} alt="home-icon"/>
-                      <div>
-                          <h6>Buy a house</h6>
-                          <p>Saving goal</p>
-                      </div>
-                  </div>
-                  <div>
-                      <div>Amount field</div>
-                      <div>Goal field</div>
-                  </div>
-                  <div>
-                      <div>
-                          <p>Monthly amount</p>
-                          <p>$520.83</p>
-                      </div>
-                      <div>
-                          <p>You’re planning <b>48 monthly deposits</b> to reach your <b>$25,000</b> goal by <b>October 2020.</b></p>
-                      </div>
-                  </div>
-                  <button>Confirm</button>
-              </div>
-          </div>
-      </StyledPage>
-  )
+    return (
+        <StyledPage>
+            <StyledHeader>
+                <img src={logoImg} alt='logo' />
+            </StyledHeader>
+            <StyledOuter>
+                <StyledInner>
+                    <StyledDescription>
+                        Let's plan your <StyledBold>saving goal.</StyledBold>
+                    </StyledDescription>
+                    <StyledBox>
+                        <StyledBoxHeader>
+                            <img src={homeImg} alt='home-icon' />
+                            <StyledBoxHeaderContent>
+                                <h6>Buy a house</h6>
+                                <p>Saving goal</p>
+                            </StyledBoxHeaderContent>
+                        </StyledBoxHeader>
+                        <StyledRow>
+                            <div>Amount field</div>
+                            <div>Goal field</div>
+                        </StyledRow>
+                        <StyledAmount>
+                            <StyledAmountTop>
+                                <p>Monthly amount</p>
+                                <StyledAmountPrice>$520.83</StyledAmountPrice>
+                            </StyledAmountTop>
+                            <StyledAmountBottom>
+                                <p>
+                                    You’re planning <StyledBold>48 monthly deposits</StyledBold> to reach your <StyledBold>$25,000</StyledBold> goal by <StyledBold>October 2020.</StyledBold>
+                                </p>
+                            </StyledAmountBottom>
+                        </StyledAmount>
+                        <StyledFooter>
+                            <StyledButton>Confirm</StyledButton>
+                        </StyledFooter>
+                    </StyledBox>
+                </StyledInner>
+            </StyledOuter>
+        </StyledPage>
+    );
 };
 
 export { HomePage };
